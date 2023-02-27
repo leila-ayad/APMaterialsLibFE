93101a60a98e51d0c30ea772cb13a2f5fb15511e
Leila <leila.alhemali@gmail.com>
2023-02-27 10:58:44 -0700

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
-import url from "url";
 
 const BASE_URL = "https://secure-gorge-57927.herokuapp.com/api";
-
-const fixieUrl = url.parse(process.env.FIXIE_URL);
-const fixieAuth = fixieUrl.auth.split(":");
 
 export default axios.create({
   baseURL: BASE_URL,
@@ -12,12 +8,6 @@ export default axios.create({
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin":
       "https://intense-island-04875.herokuapp.com/",
-  },
-  proxy: {
-    protocol: "http",
-    host: fixieUrl.hostname,
-    port: fixieUrl.port,
-    auth: { username: fixieAuth[0], password: fixieAuth[1] },
   },
 });
 
