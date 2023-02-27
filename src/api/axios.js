c1a961320f5d691208d11e88aa8b830e193f31cb
Leila <leila.alhemali@gmail.com>
2023-02-27 11:05:56 -0700

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,14 +1,13 @@
 import axios from "axios";
-
 const BASE_URL = "https://secure-gorge-57927.herokuapp.com/api";
 
 export default axios.create({
   baseURL: BASE_URL,
-  headers: {
+    headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin":
       "https://intense-island-04875.herokuapp.com/",
-  },
+  }
 });
 
 export const axiosPrivate = axios.create({
