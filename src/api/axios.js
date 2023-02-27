1427c6045889be210d1c7d4e483c050bf088ebba
Leila <leila.alhemali@gmail.com>
2023-02-27 09:41:14 -0700

    added access control header

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,21 @@
-import axios from 'axios';
-const BASE_URL = 'https://secure-gorge-57927.herokuapp.com/api';
+import axios from "axios";
+const BASE_URL = "https://secure-gorge-57927.herokuapp.com/api";
 
 export default axios.create({
-    baseURL: BASE_URL,
-})
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin":
+      "https://intense-island-04875.herokuapp.com/",
+  }
+});
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    headers: { 'Content-Type': 'application/json'},
-    withCredentials: true
-}) 
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin":
+      "https://intense-island-04875.herokuapp.com/",
+  },
+  withCredentials: true,
+});
