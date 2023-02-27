6f12c1d15a08f0701c9449030e7aac44876785e7
Leila <leila.alhemali@gmail.com>
2023-02-27 10:02:40 -0700

    added fixie url

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const app = express();const axios = require('axios');
+const url = require('url');
+const fixieUrl = url.parse(process.env.FIXIE_URL);
+const fixieAuth = fixieUrl.auth.split(':');
+
+axios.get('https://abstractpicnicmaterials.com', {
+  proxy: {
+    protocol: 'http',
+    host: fixieUrl.hostname,
+    port: fixieUrl.port,
+    auth: {username: fixieAuth[0], password: fixieAuth[1]}
+  }
+}).then(response => {
+  console.log(response.status);
+});
 
 app.use(express.static(path.join(__dirname, 'build')));
 
