a650eaa5da06a7d7dca44749a473b89feeeee54a
Shea Scott <sheadscott@gmail.com>
2023-03-11 10:33:20 -0600

    Added dev env check

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,20 +1,22 @@
-import axios from "axios";
-const BASE_URL = "https://secure-gorge-57927.herokuapp.com/api";
-
+import axios from 'axios'
+const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:9000'
+    : 'https://secure-gorge-57927.herokuapp.com/api'
 
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://www.abstractpicnicmaterials.com",
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'https://www.abstractpicnicmaterials.com',
   },
-});
+})
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "https://www.abstractpicnicmaterials.com",
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'https://www.abstractpicnicmaterials.com',
   },
   withCredentials: true,
-});
+})
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import { AuthProvider } from "./Contexts/AuthProvider";
-import { MessageProvider } from "./Contexts/MessageProvider";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import { AuthProvider } from './Contexts/AuthProvider'
+import { MessageProvider } from './Contexts/MessageProvider'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log('process.env.NODE_ENV :>> ', process.env.NODE_ENV)
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <AuthProvider> 
-      <MessageProvider>
-      <Routes>
-        <Route path="/*" element={<App/>} />
-      </Routes>
-      </MessageProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <MessageProvider>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </MessageProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
-
+)
